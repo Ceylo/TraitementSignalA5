a0135e4f51f3b84cb3318e191f5ba2d33e05799e
Viewtiful <baptiste.haudegand@gmail.com>
2013-12-06 21:55:27 +0100

    Modified and corrected the code for buttord and frezq

--- exo4.m
+++ exo4.m
@@ -17,18 +17,18 @@ clear;
 clf;
 
 fe = 8000;
-fcut_start = 1000 / (fe * 2);
-fcut_end = (fcut_start + 200) / (fe * 2);
+fcut_start = 1000 / (fe / 2);
+fcut_end = 1200 / (fe / 2);
 db1 = 1;
 db2 = 40;
 
 [n wn] = buttord(fcut_start, fcut_end, db1, db2);
 [B, A] = butter(n, wn);
 
-[H, w] = freqz(B,A) ;                   % w : pulsation entre 0 et pi
-nu = w/(2*pi) ;                         % Frequence reduite
+[H, w] = freqz(B,A, 512, 8000) ;                   % w : pulsation entre 0 et pi
+%nu = w/(2*pi) ;                         % Frequence reduite
 
-plot(nu,20*log10(abs(H))) ;
+plot(w,20*log10(abs(H))) ;
 my_title("Diagramme de gain en dB", 13) ;
 my_xlabel("Frequence reduite") ;
 
@@ -41,7 +41,7 @@ my_title("Zeros (o) et poles (x)", 13) ;
 
 % Reponse impulsionnelle (il est egalement possible d'utiliser impz)
 
-N = 250 ;
+N = 64 ;
 n0 = N/2 ;
 n_min = 1-n0 ;
 n_max = N-n0 ;
@@ -52,7 +52,7 @@ d(n0) = 1 ;
 h = filter (B, A, d) ;
 
 input ("Figure suivante ? ") ;
-h_obj = plot (n,h) ;
+h_obj = stem (n,h) ;
 set_ylim(h) ;
 set (h_obj, 'markersize', 5) ;
 my_title("Reponse impulsionnelle", 13) ;
@@ -66,6 +66,7 @@ compo_t = 0:compo_Te:1 ;
 compo_x = 0.2*sin(2*pi*800*compo_t); % sinusoide à 800 Hz
 compo_x = compo_x.+0.2*sin(2*pi*1400*compo_t); % sinusoide à 1400 Hz
 
+input ("Figure suivante ? ") ;
 plot(compo_t, compo_x) ;
 my_title ('Signal initial', 13) ;
 my_xlabel ('Temps (s)') ;
