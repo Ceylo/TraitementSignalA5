14467ed1d51427ca2f800708db02b8d3d0447055
Viewtiful <baptiste.haudegand@gmail.com>
2013-12-06 22:07:43 +0100

    Modified abs for impulsional response

--- exo4.m
+++ exo4.m
@@ -40,7 +40,7 @@ my_title("Zeros (o) et poles (x)", 13) ;
 
 % Reponse impulsionnelle (il est egalement possible d'utiliser impz)
 
-N = 64 ;
+N = 250 ;
 n0 = N/2 ;
 n_min = 1-n0 ;
 n_max = N-n0 ;
@@ -51,7 +51,7 @@ d(n0) = 1 ;
 h = filter (B, A, d) ;
 
 input ("Figure suivante ? ") ;
-h_obj = stem (n,h) ;
+h_obj = plot (n,h) ;
 set_ylim(h) ;
 set (h_obj, 'markersize', 5) ;
 my_title("Reponse impulsionnelle", 13) ;
